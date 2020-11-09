8d75cc6d4de6400093fa991e1810007f027bb03c
programmernub <emanuel00perez@gmail.com>
2020-11-08 18:36:03 -0600

    js changes, general form validated

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,17 @@
 const btnEnviar = document.getElementById("btn-enviar");
-const form = {
-	nombre: document.getElementById("nombre.value"),
-	correo: document.getElementById("correo.value"),
-	producto: document.getElementById("producto.value"),
-	description: document.getElementById("description.value")
-};
 btnEnviar.addEventListener('click', (e)=>{
 	e.preventDefault();
+	const form = {
+		nombre: document.getElementById("nombre").value,
+		correo: document.getElementById("correo").value,
+		producto: document.getElementById("producto").value,
+		description: document.getElementById("descripcion").value
+	};
 	validar(form);	
 });
 
 function validar(form){
+	console.log(form);
 	if (form.nombre === "" || form.correo === "" || form.producto === "" || form.description === "") {
 		alert("Ningún campo puede quedar en blanco");
 	}else{
